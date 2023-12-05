50587ffe133be1c16cd68ff432eec4f3ff81e070
BillSteinUNB <113315555+BillSteinUNB@users.noreply.github.com>
2023-12-05 12:13:58 -0400

    Apiissues

--- app/src/main/java/ca/unb/mobiledev/weatherwizard/LaunchActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/weatherwizard/LaunchActivity.kt
@@ -17,8 +17,15 @@ import com.google.android.gms.location.LocationServices
 import androidx.core.app.ActivityCompat
 import android.Manifest
 import android.content.pm.PackageManager
+import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+
+
 
 
 
@@ -28,60 +35,92 @@ import com.android.volley.toolbox.Volley
 class LaunchActivity : ComponentActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var textView: TextView
-    private lateinit var weatherTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launch)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        weatherTextView = findViewById(R.id.weatherTextView)
 
-        getLocationAndWeather()
+        // State for weather text
+        val weatherText = mutableStateOf("Fetching weather...")
+        val hardcodedWeatherData = " 15 degrees Celsius"
+
+        setContent {
+            WeatherWizardTheme {
+                WeatherScreen(city ="fredericton", temperature = hardcodedWeatherData )
+            }
+        }
+
+        getLocationAndWeather(weatherText)
     }
 
-    private fun getLocationAndWeather() {
+    private fun getLocationAndWeather(weatherText: MutableState<String>) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Request location permission if not granted
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
             return
         }
 
         fusedLocationClient.lastLocation.addOnSuccessListener { location ->
             if (location != null) {
-                val weatherUrl = "https://api.weatherbit.io/v2.0/current?lat=${location.latitude}&lon=${location.longitude}&key=YOUR_API_KEY"
-                getCurrentWeather(weatherUrl)
+                val weatherUrl = "https://api.weatherbit.io/v2.0/current?lat=${location.latitude}&lon=${location.longitude}&key=d48fe7c323934ed0bb8c26854b3d9e0a"
+                getCurrentWeather(weatherUrl, weatherText)
             }
         }
     }
 
-    private fun getCurrentWeather(url: String) {
+    private fun getCurrentWeather(url: String, weatherText: MutableState<String>) {
         val queue = Volley.newRequestQueue(this)
         val stringReq = StringRequest(Request.Method.GET, url, { response ->
             val obj = JSONObject(response)
             val arr = obj.getJSONArray("data")
             val obj2 = arr.getJSONObject(0)
-            val weatherText = "The temperature in ${obj2.getString("city_name")} is ${obj2.getString("temp")} degrees Celsius"
-            textView.text = weatherText
-        }, { textView.text = "Error fetching weather data" })
+            weatherText.value = "The temperature in ${obj2.getString("city_name")} is ${obj2.getString("temp")} degrees Celsius"
+        }, { weatherText.value = "Error fetching weather data" })
         queue.add(stringReq)
     }
-}
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun WeatherScreen(city: String, temperature: String) {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = city,
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Temperature: $temperature",
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherWizardTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun WeatherScreenPreview() {
+        WeatherWizardTheme {
+            WeatherScreen(city = "Fredericton", temperature = "15 degrees Celsius")
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(text = "Hello $name!", modifier = modifier)
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        WeatherWizardTheme {
+            Greeting("Android")
+        }
     }
 }
