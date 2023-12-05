6f3e943965b2c927101c3a7026834b723442d743
BillSteinUNB <wstein@unb.ca>
2023-12-04 23:52:30 -0400

    gradle sucks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.0.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 buildscript {
-    repositories {
-        google() // This line should be included
-        // Other repositories...
-    }
-    // ...
+
+
 }
 
 allprojects {
